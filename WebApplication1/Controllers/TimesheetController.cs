1f6e42b3a6c2aa2f000408f8cc0dce2a72915743
Chirag github <chiragvaland22@gmail.com>
2020-08-03 21:51:22 -0500

    added new Feature Timesheet

---
+++ WebApplication1/Controllers/TimesheetController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TimesheetController : Controller
+    {
+        // GET: Timesheet
+        public ActionResult Random()
+        {
+            var timesheet = new Timesheet() { CompanyName = "ABC" };
+
+
+            return View(timesheet);
+        }
+    }
+}
---
+++ WebApplication1/Models/Timesheet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Timesheet
+    {
+        public int TimesheetNo { get; set; }
+        public string CompanyName { get; set; }
+        public string EmployeeName { get; set; }
+        public string WeekOf { get; set; }
+        public string ClockinDate { get; set; }
+        public string ClockinDay { get; set; }
+        public string Intime { get; set; }
+        public string OutTime { get; set; }
+        public int DailyTotal { get; set; }
+        public int WeeklyTotal { get; set; }
+    }
+}
